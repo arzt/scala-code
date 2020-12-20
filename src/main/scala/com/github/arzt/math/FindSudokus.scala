ea78aef8c64ff9adf8712c76b0ae7895bab43ba1
sebastian <sebastian@simplaex.com>
2020-12-20 16:27:31 +0100

    add find sudoku entrypoint

---
+++ src/main/scala/com/github/arzt/math/FindSudokus.scala
@@ -0,0 +1,27 @@
+package com.github.arzt.math
+
+object FindSudokus {
+  def main(args: Array[String]): Unit = {
+    println("test")
+    val s = new Sudoku(3, 3)
+    val a = s.initArray()
+    var i = 1
+    var countAll = 0L
+    var countSudoku = 0L
+    while (i > 0) {
+      //s.printSudoku(a)
+      i = s.nextCandidate(a, i, s.isValidSudoku)
+      if (s.isValidSudoku(a)(s.cellCount)) {
+        countSudoku += 1
+        println(countSudoku)
+        if (countSudoku % 10000 == 0) {
+          val ratio = 1.0*countSudoku/countAll
+          println(f"$ratio ($countSudoku/$countAll)")
+          println(ratio)
+          println(a.mkString(""))
+        }
+      }
+      countAll += 1
+    }
+  }
+}
